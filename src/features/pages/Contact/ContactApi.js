0eaab2e499df844cbadae4cb608dca5af04be947
devenamul <devenamul@gmail.com>
2025-04-13 13:19:20 +0600

    some change

--- src/features/pages/Contact/ContactApi.js
+++ src/features/pages/Contact/ContactApi.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 export const createContactData = createAsyncThunk(
     "contact/createContactData",
     async (data) => {
-      const response = await axios.post("https://personal-portfolio-backend-e0vp.onrender.com/createData", data);
+      const response = await axios.post("http://localhost:5050/createData", data);
       console.log(response);
       
       return response.data;
