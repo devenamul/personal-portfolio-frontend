b21efd518979ba9285afb0cf5d9bce293c25ac1e
devenamul <devenamul@gmail.com>
2025-04-13 14:23:22 +0600

    some change

--- src/features/pages/Contact/ContactApi.js
+++ src/features/pages/Contact/ContactApi.js
@@ -2,12 +2,14 @@
 // get all user data
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios'
+import.meta.env.VITE_VENDER_API_BASE_URL
+
 
 
 export const createContactData = createAsyncThunk(
     "contact/createContactData",
     async (data) => {
-      const response = await axios.post("http://localhost:5050/createData", data);
+      const response = await axios.post(`${import.meta.env.VITE_VENDER_API_BASE_URL}/createData`, data);
       console.log(response);
       
       return response.data;
